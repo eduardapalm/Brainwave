ad67faf5cd700d853cf5ff7c846f936799b1d3cb
Maria Eduarda P. de Almeida <eduardapalm27@gmail.com>
2024-05-19 00:32:59 -0300

    refactor: Improve animation and loading indicators

--- src/components/Collaboration.jsx
+++ src/components/Collaboration.jsx
@@ -54,7 +54,7 @@ const Collaboration = () => {
                   key={item.id}
                   className={`absolute top-0 left-1/2 h-1/2 -ml-[1.6rem] origin-bottom rotate-${
                     index * 45
-                  }`}
+                  } animate-pulse`}
                 >
                   <div
                     className={`relative -top-[1.6rem] flex w-[3.2rem] h-[3.2rem] bg-n-7 border border-n-1/15 rounded-xl -rotate-${
