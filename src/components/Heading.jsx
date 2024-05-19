af83b85554ee4927b8fc5941ea151f91f2e3fe5d
Maria Eduarda P. de Almeida <eduardapalm27@gmail.com>
2024-05-19 02:50:13 -0300

    feat: Add Pricing component and update Heading component

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -1,8 +1,33 @@
-const Heading = ({ className, title, text }) => {
+import Tagline from './Tagline';
+
+const Heading = ({
+  className,
+  title,
+  titleClasses,
+  text,
+  textClasses,
+  tag,
+  tagClasses,
+}) => {
   return (
-    <div className={`${className} max-w-[50rem] mx-auto mb-12 lg:mb-20`}>
-      {title && <h2 className="h2">{title}</h2>}
-      {text && <p className="body-2 mt-4 text-n-4 lg:text-center">{text}</p>}
+    <div
+      className={`${className} max-w-[50rem] mx-auto mb-12 lg:mb-20 md:text-center`}
+    >
+      {tag && (
+        <Tagline
+          className={`mb-4 md:justify-center ${tagClasses && tagClasses}`}
+        >
+          {tag}
+        </Tagline>
+      )}
+      {title && (
+        <h2 className={`h2 ${titleClasses && titleClasses}`}>{title}</h2>
+      )}
+      {text && (
+        <p className={`body-2 mt-4 text-n-4 ${textClasses && textClasses}`}>
+          {text}
+        </p>
+      )}
     </div>
   );
 };
